7278c4c1b23fd896000eaad0fdf7aa6a0ddc01dd
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-11-24 19:30:21 +0500

    Task4

---
+++ Tyuiu.KarpovAA.Sprint5.Task4.V24.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KarpovAA.Sprint5.Task4.V24.Lib;
+using System.IO;
+
+
+namespace Tyuiu.KarpovAA.Sprint5.Task4.V24.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask4V24.txt";
+            FileInfo fileinfo = new FileInfo(path);
+            bool FileExists = fileinfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, FileExists);
+        }
+    }
+}
---
+++ Tyuiu.KarpovAA.Sprint5.Task4.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KarpovAA.Sprint5.Task4.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KarpovAA.Sprint5.Task4.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3576e52b-7960-406d-98fa-8d83f7be4669")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
