767e52da81500cca0c2e35e965efc352756393bd
Андрей Карпов <karpov.andrei.000@mail.ru>
2023-11-24 19:43:39 +0500

    Task5

---
+++ Tyuiu.KarpovAA.Sprint5.Task5.V26/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.KarpovAA.Sprint5.Task5.V26.Lib;
+using System.IO;
+
+namespace Tyuiu.KarpovAA.Sprint5.Task5.V26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+
+
+            Console.Title = "Спринт #5 | Выполнил: Карпов А. А. | ИИПб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Обработка файлов                                                  *");
+            Console.WriteLine("* Задание #5                                                              *");
+            Console.WriteLine("* Вариант 26                                                              *");
+            Console.WriteLine("* Выполнил: Карпов Андрей Андреевич | ИИПб-23-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан файл С:DataSprint5InPutDataFileTask5V26.txt (файл взять из архива   *");
+            Console.WriteLine("* согласно вашему варианту. Создать папку в ручную С: DataSprint5 и       *");
+            Console.WriteLine("* скопировать в неё файл) в котором есть набор значений. Найти разницу    *");
+            Console.WriteLine("* между суммой всех  положительных и отрицательных вещественных чисел в   *");
+            Console.WriteLine("* файле. Результат округлить до 3 знаков после запятой. Полученный        *");
+            Console.WriteLine("* результат вывести на консоль. У вещественных значений округлить до      *");
+            Console.WriteLine("* трёх знаков после запятой.                                              *"); 
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask5V26.txt";
+
+            Console.WriteLine("Данные находятся в файле" + path);
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            double res = ds.LoadFromDataFile(path);
+
+            Console.WriteLine(res);
+
+
+            Console.ReadKey();
+        }
+    }
+}
